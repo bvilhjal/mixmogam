03e8fd32e698d868654697a9427a6b2ef943600a
Bjarni Vilhjalmsson <bjarni.vilhjalmsson@gmail.com>
2013-09-09 14:24:19 -0400

    ability to work with HDF5 files added

--- hdf5_data.py
+++ hdf5_data.py
@@ -14,32 +14,40 @@ def calculate_ibd_kinship(hdf5_filename='/home/bv25/data/Ls154/Ls154_12.hdf5',
     """
     
     h5f = h5py.File(hdf5_filename)
-    c22g = h5f['chrom_22']
-    n_indivs = len(c22g['raw_snps'][0])
+    n_indivs = len(h5f['indiv_data']['indiv_ids'][...])
     if overwrite or not 'kinship' in h5f.keys():
         if 'kinship' in h5f.keys():
             print 'Overwriting kinship.'
         print 'Calculating kinship.'
         k_mat = sp.zeros((n_indivs, n_indivs), dtype='single')
         
-        n_snps = 0
-        for chrom in range(1, 6):
-            chrom_str = 'chrom_%d' % chrom
-            print 'Working on chromosome %d' % chrom
-            cg = h5f[chrom_str]
-            num_snps = len(cg['snps'])
-            assert len(cg['snps']) == len(cg['raw_snps']), 'WTF?'
+        gg = h5f['genot_data']
+        chromosomes = gg.keys()
+        for chrom in chromosomes:
+            print 'Working on Chromosome %s' % chrom
+            cg = gg[chrom]
+            num_snps = len(cg['raw_snps'])
+            normalized = 'snps' in cg.keys()                
+            n_snps = 0
+            
             for chunk_i, i in enumerate(range(0, num_snps, chunk_size)):
     #            if chunk_i % 2 != 0:
     #                continue
                 end_i = min(i + chunk_size, num_snps)
-                x = cg['snps'][i:end_i]
+                if normalized:
+                    x = cg['snps'][i:end_i]
+                else:
+                    x = cg['raw_snps'][i:end_i]
+                    x = x.T
+                    x = (x - sp.mean(x, 0)) / sp.std(x, 0)
+                    x = x.T           
                 n_snps += len(x)
                 k_mat += sp.dot(x.T, x)
                 del x
                 sys.stdout.write('\b\b\b\b\b\b\b%0.2f%%' % (100.0 * (min(1, ((chunk_i + 1.0) * chunk_size) / num_snps))))
                 sys.stdout.flush()
             sys.stdout.write('\b\b\b\b\b\b\b100.00%\n')
+            assert n_snps == num_snps, 'WTF?'
         k_mat = k_mat / float(n_snps)
         h5f.create_dataset('kinship', data=k_mat)
     else:
