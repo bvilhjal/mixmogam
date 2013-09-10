c25c9a7096e5c6efbfc6a9867da86fa003547761
Bjarni Vilhjalmsson <bjarni.vilhjalmsson@gmail.com>
2013-09-10 14:30:36 -0400

    ability to work with HDF5 files added

--- hdf5_data.py
+++ hdf5_data.py
@@ -225,9 +225,9 @@ def manhattan_plot(hdf5_results_file='/home/bv25/data/Ls154/Ls154_results.hdf5',
             plt.plot(plot_positions, log_ps, ".", markersize=markersize, alpha=0.7, color=color, mew=0)
 
         chrom_end = chrom_res_dict[chrom]['chrom_end']
-        for j in range(offset, offset + chrom_end, 10000000):
+        for j in range(offset, offset + chrom_end, 5000000):
             tick_positions.append(j)
-            if  j % 20000000 == 0 and j < offset + chrom_end - 5000000 :
+            if  j % 10000000 == 0 and j < offset + chrom_end - 5000000 :
                 tick_strings.append(str(j / 1000000))
             else:
                 tick_strings.append('')
